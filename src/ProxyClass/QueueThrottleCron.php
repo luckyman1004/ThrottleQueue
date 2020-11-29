f6410b6db110c644fc713a4b08f84e423aeb21f8
luckyman1004 <luckyman.sftdev@gmail.com>
2020-11-29 14:02:09 +0000

    Issue #3148534 by Project Update Bot: Automated Drupal 9 compatibility fixes

--- src/ProxyClass/QueueThrottleCron.php
+++ src/ProxyClass/QueueThrottleCron.php
@@ -7,15 +7,18 @@
 
 namespace Drupal\queue_throttle\ProxyClass {
 
+    use Drupal\Core\CronInterface;
+    use Drupal\Core\DependencyInjection\DependencySerializationTrait;
+    use Symfony\Component\DependencyInjection\ContainerInterface;
     /**
      * Provides a proxy class for \Drupal\queue_throttle\QueueThrottleCron.
      *
      * @see \Drupal\Component\ProxyBuilder
      */
-    class QueueThrottleCron implements \Drupal\Core\CronInterface
+    class QueueThrottleCron implements CronInterface
     {
 
-        use \Drupal\Core\DependencyInjection\DependencySerializationTrait;
+        use DependencySerializationTrait;
 
         /**
          * The id of the original proxied service.
@@ -46,7 +49,7 @@ namespace Drupal\queue_throttle\ProxyClass {
          * @param string $drupal_proxy_original_service_id
          *   The service ID of the original service.
          */
-        public function __construct(\Symfony\Component\DependencyInjection\ContainerInterface $container, $drupal_proxy_original_service_id)
+        public function __construct(ContainerInterface $container, $drupal_proxy_original_service_id)
         {
             $this->container = $container;
             $this->drupalProxyOriginalServiceId = $drupal_proxy_original_service_id;
